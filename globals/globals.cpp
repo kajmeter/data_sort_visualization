c68fc74cbab9723d1d46884ccb17a75dbe849d2f
kajmeter <85412627+kajmeter@users.noreply.github.com>
2023-04-13 13:46:20 +0200

    Create globals.cpp

---
+++ globals/globals.cpp
@@ -0,0 +1,11 @@
+
+#include "globals.h"
+#include <iostream>
+#include <string>
+using namespace std;
+namespace Globals{
+    int arraySort[1000];
+    int usToDelay = 30;
+    int globali;
+    bool action = false;
+}
