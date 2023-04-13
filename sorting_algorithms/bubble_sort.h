c61e11d26e055117170a4401cf5ad45f673ffcda
kajmeter <85412627+kajmeter@users.noreply.github.com>
2023-04-13 13:45:02 +0200

    Create bubble_sort.h

---
+++ sorting_algorithms/bubble_sort.h
@@ -0,0 +1,20 @@
+
+#ifndef DATASORTVISUALP_BUBBLE_SORT_H
+#define DATASORTVISUALP_BUBBLE_SORT_H
+
+#include <string>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <unistd.h>
+#include <ctime>
+using namespace std;
+using namespace sf;
+class bubble_sort {
+
+public:
+    bubble_sort(int chunks);
+};
+
+
+#endif //DATASORTVISUALP_BUBBLE_SORT_H
