da010d696ce1b362853c5ec25223a41265a196b2
kajmeter <85412627+kajmeter@users.noreply.github.com>
2023-04-13 13:46:56 +0200

    Create globals.h

---
+++ globals/globals.h
@@ -0,0 +1,15 @@
+
+#ifndef DATASORTVISUALP_GLOBALS_H
+#define DATASORTVISUALP_GLOBALS_H
+#include <iostream>
+class globals {};
+
+namespace Globals{
+    extern int arraySort[1000];
+    extern int usToDelay;
+    extern int globali;
+    extern bool action;
+}
+
+
+#endif //DATASORTVISUALP_GLOBALS_H
